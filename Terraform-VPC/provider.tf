e8cfa40e0801854a396532e4489e2a31e10e8d14
George Akanza <gakanza@gmail.com>
2025-01-04 13:10:52 -0500

    source code

---
+++ Terraform-VPC/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.0.0"
+    }
+  }
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "fluentindevops-tf-statefile"
+    key    = "fluentindevops-tf-statefile/eks/statefile"
+    region = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "fluentindevops-tf-state-lock"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
+
+
+
